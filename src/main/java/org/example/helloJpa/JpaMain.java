d62214f4276697bbdd119d2e39911d0ab707ccbf
khlee <panpan68@naver.com>
2023-05-04 18:57:17 +0900

    day06 - 값타입 컬렉션

--- src/main/java/org/example/helloJpa/JpaMain.java
+++ src/main/java/org/example/helloJpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
@@ -17,34 +20,10 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeaddress(new Address("homeCity", "street", "10000"));
+                Member member = new Member();
+                member.setUsername("member1");
+                em.persist(member);
 
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1","street","10000"));
-            member.getAddressHistory().add(new AddressEntity("old2","street","10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-//            //값타입은 변하면 안된다.
-            Member member1 = em.find(Member.class, member.getId());
-//            Address a = member1.getHomeaddress();
-//            member1.setHomeaddress(new Address("newOne", a.getStreet(), a.getCity()));
-//
-//            //치킨 -> 한식
-//            member1.getFavoriteFoods().remove("치킨");
-//            member1.getFavoriteFoods().add("한식");
-//
-//            //equals, hashCode 오버라이딩을 제대로 해야하는 이유
-//            member1.getAddressHistory().remove(new AddressEntity("old1","street","10000"));
-//            member1.getAddressHistory().add(new AddressEntity("NewNew","street","10000"));
 
             tx.commit();
         }catch (Exception e){
